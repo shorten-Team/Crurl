6b77e716c18d339b5263ebac55841a2c2e0b3d52
naserteam <janjo.urmia2@gmail.com>
2016-02-04 16:18:46 +0330

    Update all.lua

--- plugins/all.lua
+++ plugins/all.lua
@@ -23,7 +23,7 @@ local function chat_stats(chat_id)
         return a.msgs > b.msgs
       end
     end)
-  local text = 'Chat stats:\n'
+  local text = 'وضعیت گروه:\n'
   for k,user in pairs(users_info) do
     text = text..user.name..' = '..user.msgs..'\n'
   end
@@ -34,7 +34,7 @@ local function get_group_type(target)
   local data = load_data(_config.moderation.data)
   local group_type = data[tostring(target)]['group_type']
     if not group_type or group_type == nil then
-       return 'No group type available.'
+       return 'نوع گروه مشخص نیست'
     end
       return group_type
 end
@@ -49,7 +49,7 @@ local function show_group_settings(target)
     end
   end
   local settings = data[tostring(target)]['settings']
-  local text = "Lock group name : "..settings.lock_name.."\nLock group photo : "..settings.lock_photo.."\nLock group member : "..settings.lock_member.."\nflood sensitivity : "..NUM_MSG_MAX
+  local text = "قفل نام : "..settings.lock_name.."\nقفل عکس: "..settings.lock_photo.."\nقفل عضوگیری "..settings.lock_member.."\nآنتی اسپم : "..NUM_MSG_MAX
   return text
 end
 
@@ -57,7 +57,7 @@ local function get_description(target)
   local data = load_data(_config.moderation.data)
   local data_cat = 'description'
   if not data[tostring(target)][data_cat] then
-    return 'No description available.'
+    return 'توضیحی تنظیم نشده است'
   end
   local about = data[tostring(target)][data_cat]
   return about
@@ -67,7 +67,7 @@ local function get_rules(target)
   local data = load_data(_config.moderation.data)
   local data_cat = 'rules'
   if not data[tostring(target)][data_cat] then
-    return 'No rules available.'
+    return 'قانونی تنظیم شده است'
   end
   local rules = data[tostring(target)][data_cat]
   return rules
@@ -78,10 +78,10 @@ local function modlist(target)
   local data = load_data(_config.moderation.data)
   local groups = 'groups'
   if not data[tostring(groups)] or not data[tostring(groups)][tostring(target)] then
-    return 'Group is not added or is Realm.'
+    return 'گروه در سرور اضافه نشده است\nیا گروه ادمین است'
   end
   if next(data[tostring(target)]['moderators']) == nil then
-    return 'No moderator in this group.'
+    return 'لیست مدیران \n_____________________________\nدرحال حاضر مدیری وجود ندارد.\nصاحب گروه مجزا است'
   end
   local i = 1
   local message = '\nList of moderators :\n'
@@ -96,29 +96,29 @@ local function get_link(target)
   local data = load_data(_config.moderation.data)
   local group_link = data[tostring(target)]['settings']['set_link']
   if not group_link or group_link == nil then 
-    return "No link"
+    return "لینکی وجود ندارد"
   end
-  return "Group link:\n"..group_link
+  return "لینک دعوت به گروه:\n"..group_link
 end
 
 local function all(target, receiver)
-  local text = "All the things I know about this group\n\n"
+  local text = "تمامی تنظیمات و وضعیت گروه بدین شرح است\n\n"
   local group_type = get_group_type(target)
-  text = text.."Group Type: \n"..group_type
+  text = text.."نوع گروه: \n"..group_type
   local settings = show_group_settings(target)
-  text = text.."\n\nGroup settings: \n"..settings
+  text = text.."\nاتظیمات گروه: \n"..settings
   local rules = get_rules(target)
-  text = text.."\n\nRules: \n"..rules
+  text = text.."\nقوانین: \n"..rules
   local description = get_description(target)
-  text = text.."\n\nAbout: \n"..description
+  text = text.."\nتوضیحات: \n"..description
   local modlist = modlist(target)
-  text = text.."\n\nMods: \n"..modlist
+  text = text.."\nمدیران: \n"..modlist
   local link = get_link(target)
-  text = text.."\n\nLink: \n"..link
+  text = text.."\nلینک گروه: \n"..link
   local stats = chat_stats(target)
-  text = text.."\n\n"..stats
+  text = text.."\n"..stats
   local ban_list = ban_list(target)
-  text = text.."\n\n"..ban_list
+  text = text.."\n"..ban_list
   local file = io.open("./groups/all/"..target.."all.txt", "w")
   file:write(text)
   file:flush()
