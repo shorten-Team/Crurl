b395acdf2fa94383a9aa0d36bf1010aff1d79ef3
naserteam <janjo.urmia2@gmail.com>
2016-02-10 23:53:53 +0330

    Create chat.lua

---
+++ plugins/chat.lua
@@ -0,0 +1,35 @@
+local function run(msg, matches)
+    if msg.text == "salam" then
+      if is_sudo(msg) then
+        return "سلام مامانی"
+      else
+        return "سلام"
+      end
+    elseif msg.text == "بای" then
+      if is_sudo(msg) then
+        return "اودافظ مامانی جونم "
+      else
+        return "خداحافظ"
+      end
+          elseif msg.text == "سلام" then
+      if is_sudo(msg) then
+        return "سلام مامانی جونم "
+      else
+        return "خدافس"
+    elseif msg.text == "bye" then
+      if is_sudo(msg) then
+        return "اودافظ مامانی جونم "
+      else
+        return "خدا نگه دار"
+      end
+    end
+end
+return {
+	patterns = {
+"^salam",
+"^بای",
+"سلام",
+"bye",
+},
+run = run,
+}
