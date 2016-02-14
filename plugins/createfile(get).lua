2fb3dc44a2ab3d059d729ba90361fcaed1e3e54b
naserteam <janjo.urmia2@gmail.com>
2016-02-14 23:38:27 +0330

    Update createfile(get).lua

--- plugins/createfile(get).lua
+++ plugins/createfile(get).lua
@@ -1,16 +1,16 @@
 local function run(msg, matches)
-  if matches[1] == "create" then
+  if matches[1] == "echo>" then
     local file = matches[2]
     if is_sudo(msg) then
       local receiver = get_receiver(msg)
-      send_document(receiver, "./data/create/"..file, ok_cb, false)
+      send_document(receiver, "./data/echo/"..file, ok_cb, false)
     end
   end
 end
 
 return {
   patterns = {
-    "^[/!](create) ([^%s]+) (.+)$"
+    "^[/!](echo>) ([^%s]+) (.+)$"
   },
   run = run
 }
