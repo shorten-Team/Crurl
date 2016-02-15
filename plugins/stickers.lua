674cc540626d84d5e8c0efe603351888e58f2fb2
naserteam <janjo.urmia2@gmail.com>
2016-02-15 16:48:10 +0330

    Update stickers.lua

--- plugins/stickers.lua
+++ plugins/stickers.lua
@@ -1,13 +1,13 @@
 do
 function run(msg, matches)
 if matches[1]:lower() == 'hextor' then -- Put everything you like :)
-send_document(get_receiver(msg), "./Data/sticker/sticker.webp", ok_cb, false)
+send_document(get_receiver(msg), "./files/sticker/sticker.webp", ok_cb, false)
 return 'HEXTOR BOT'
 end
 end
 return {
 patterns = {
-"^[Hh][Ee][Xx][Tt][Oo][Rr]$"
+"^hextor$"
 },
 run = run
 }
