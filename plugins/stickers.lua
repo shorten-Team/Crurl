f13cff6d81b2781998384ece035b6a117d8530c1
mohamadkhoshnava <mohamadkhoshnava@users.noreply.github.com>
2016-02-25 01:30:23 +0330

    Update stickers.lua

--- plugins/stickers.lua
+++ plugins/stickers.lua
@@ -2,12 +2,12 @@ do
 function run(msg, matches)
 if matches[1]:lower() == 'hextor' then -- Put everything you like :)
 send_document(get_receiver(msg), "./files/sticker/sticker.webp", ok_cb, false)
-return 'HEXTOR BOT'
+return 'CRUEL BOT'
 end
 end
 return {
 patterns = {
-"^hextor$"
+"^cruel$"
 },
 run = run
 }
