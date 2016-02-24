ae11d7c683d5d6c33caa4d421062bc7d6c3d352f
mohamadkhoshnava <mohamadkhoshnava@users.noreply.github.com>
2016-02-25 01:20:45 +0330

    Update and rename plugins/hextor(about).lua to cruel_about.lua

---
+++ cruel_about.lua
@@ -0,0 +1,30 @@
+do
+
+function run(msg, matches)
+  return [[
+  👒cruelbot‮‮👒™
+~~~~~~~~~~~
+👥Sudo:@cruel0098
+÷÷÷÷÷÷÷÷÷÷÷÷
+Ⓜ️admins:
+⛑️@cruel0098
+〽@Cruel_admin
+@ShayanHaddadian2
+@mosy15
+@xx_mersad_xx
+🔰برای خرید گروه به ادمین ها مراجعه کنید🔰
+
+😎موفق باشید
+]]
+  end
+return {
+  description = "Invite bot into a group chat", 
+  usage = " !cruel: نمایش مشخصات ربات",
+  patterns = {
+    "^!cruel$",
+    "^!cruel$"
+
+  },
+  run = run
+}
+end
--- plugins/hextor(about).lua
+++
@@ -1,27 +0,0 @@
-do
-
-function run(msg, matches)
-  return [[
-  👒HEXTOR‮‮👒™
-~~~~~~~~~~~
-👥Sudo:@minaco
-÷÷÷÷÷÷÷÷÷÷÷÷
-Ⓜ️admins:
-⛑️@ali_ghoghnoo
-〽@dadasalar 
-
-💢برای خرید گروه با ربات قدرتمند HEXTOR به سودو ها مراجعه کنید 💢
-
-]]
-  end
-return {
-  description = "Invite bot into a group chat", 
-  usage = " !hexto: نمایش مشخصات ربات",
-  patterns = {
-    "^![Hh]ehtor$",
-    "^![Hh]extorteam$"
-
-  },
-  run = run
-}
-end
