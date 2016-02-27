e97c7ba39c4038d496a56f4c9ec416ae0253c265
mohamadkhoshnava <mohamadkhoshnava@users.noreply.github.com>
2016-02-27 12:26:11 +0330

    Create ss.lua

---
+++ plugins/ss.lua
@@ -0,0 +1,9 @@
+sendSticker = function(chat_id, sticker, reply_to_message_id)
+
+	local url = BASE_URL .. '/sendSticker'
+
+	local curl_command = 'curl -s "' .. url .. '" -F "chat_id=' .. chat_id .. '" -F "sticker=@' .. sticker .. '"'
+
+	if reply_to_message_id then
+		curl_command = curl_command .. ' -F "reply_to_message_id=' .. reply_to_message_id .. '"'
+	end
