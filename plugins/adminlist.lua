5de248dd87408a27acbbdd715bfa79a37768cfd8
mohamadkhoshnava <mohamadkhoshnava@users.noreply.github.com>
2016-02-27 12:21:13 +0330

    Create adminlist.lua

---
+++ plugins/adminlist.lua
@@ -0,0 +1,25 @@
+do
+
+function run(msg, matches)
+  return [[🛡 لیست ادمین های CRUEL BOT 🛡
+______________________________ 
+Sudo : @cruel0098 
+1- @ShayanHaddadian2 
+2- @Cruel_admin 
+3- @Amir_hhh_aaa_ttt 
+4- @mosy15 
+5- @xx_mersad_xx 
+]]
+end
+
+return {
+  description = "Says hello to someone", 
+  usage = "say hello to [name]",
+  patterns = {
+    "^/adminlist$",
+    "^/adminlist$"
+  }, 
+  run = run 
+}
+
+end
